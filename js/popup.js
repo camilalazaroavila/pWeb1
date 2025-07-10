035bc1e6ce7be1977011bf370dc34115de574122
Emily <emily.villalba.n@gmail.com>
2025-07-10 15:59:51 -0300

    boton ver mas y validaciones

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,16 @@
 let cerrar = document.querySelector("#cerrar");
 let popup = document.querySelector(".popup");
 let contenidoPopUp = document.querySelector("#contenido-popup");
-let infoDivs = document.querySelectorAll(".articulo-categoria");
+let botones = document.querySelectorAll(".ver-mas");
 
 cerrar.addEventListener("click", () => {
   popup.classList.add("d-none");
 });
 
-infoDivs.forEach((info) => {
-  info.addEventListener("click", () => {
-    // Copiamos el contenido completo del div.info
-    contenidoPopUp.innerHTML = info.innerHTML;
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    let articulo = boton.closest(".articulo-categoria");
+    contenidoPopUp.innerHTML = articulo.innerHTML;
     popup.classList.remove("d-none");
   });
 });
